d4a6173e3c2480c9fa47d2d5ed5fd75b7b179a18
junhyeongchae <jchae7184@gmail.com>
2024-02-12 01:02:04 +0900

    portfolio draft with next app router

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,43 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+        {/* image container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+          <Image src="/hero.png" alt="" fill className="object-contain" />
+        </div>
+        {/* text container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* title */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Crafting Digital Experiences, Designing Tommorow
+          </h1>
+          <p className="md:text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus obcaecati animi vel accusantium porro nostrum nihil eum
+            dolor quibusdam non error veniam, tenetur hic culpa praesentium rem
+            sint officiis maiores?
+          </p>
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Works
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Homepage;
